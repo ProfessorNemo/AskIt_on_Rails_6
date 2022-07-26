1b622de1e3143c1e60b1aac67183cebd75c6b95f
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-07-26 19:14:22 +0300

    lesson_3

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
-  validates :body, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 2 }
 
   # "self" перед created_at не говорим, потому что метод вызывается
   # относительно конкретного образца класса
