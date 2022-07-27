65062cd65357efac043b06a310c817a6f5c4ce56
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-07-27 19:17:12 +0300

    lesson_5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[show destroy edit update]
 
   def show
-    @question = Question.find_by id: params[:id]
+    @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
+    # Answer.where(question: @question).limit(2).order(created_at: :desc)
   end
 
   def index
@@ -12,12 +15,9 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
-  def edit
-    @question = Question.find_by id: params[:id]
-  end
+  def edit; end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
       flash[:success] = 'Question updated!'
       redirect_to questions_path
@@ -27,7 +27,6 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
     @question.destroy
     flash[:success] = 'Question deleted!'
     redirect_to questions_path
@@ -49,5 +48,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question!
+    @question = Question.find params[:id]
   end
 end
