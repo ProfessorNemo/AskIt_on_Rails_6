c94993d0b7944f38998256086fe5ea23cd7917a9
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-08-02 16:56:09 +0300

    lesson_13 v.1

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
