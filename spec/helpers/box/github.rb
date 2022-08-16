82f87f506ddbef6b1bc1ad6559dd68568f75502d
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-08-17 02:57:25 +0300

    tests v.2

--- spec/helpers/box/github.rb
+++ spec/helpers/box/github.rb
@@ -4,6 +4,7 @@ module Box
   module Github
     def self.user(name)
       url  = "https://api.github.com/users/#{name}"
+
       data = Faraday.get(url).body
 
       JSON.parse(data, symbolize_names: true)
--- spec/helpers/box/tags_json.rb
+++ spec/helpers/box/tags_json.rb
@@ -3,8 +3,8 @@
 module Box
   module TagsJson
     def self.tags
-			# byebug
       url  = 'http://127.0.0.1:3000/api/tags'
+
       data = Faraday.get(url).body
 
       JSON.parse(data, symbolize_names: true)
